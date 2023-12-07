525a2f73e8cfc859959a722a203d499f8530bdb3
Colin Steele <70177066+Zenigatasan@users.noreply.github.com>
2023-12-07 00:04:08 -0800

    Update FinalWebsite.py
    Added a view for taking at least the value of a imputed currency compared to the US dollar.

--- FinalWebsite.py
+++ FinalWebsite.py
@@ -3,14 +3,24 @@ from image_info import image_info
 import random
 from flask_bootstrap import Bootstrap
 from PIL import Image
-
+from forex_python.converter import CurrencyRates
 
 app = Flask(__name__)
 
+def getrate(currency):
+    c = CurrencyRates()
+    conversion = float(c.get_rate('USD', currency))
+    return conversion
+    
 @app.route('/')
 def home():
     return render_template('index.html')
-
+    
+@app.route('/conversion/<select>')
+def conversion(select):
+    num = getrate(select)
+    return render_template('conversion.html', vals=num, select=select)
+    
 if __name__ == '__main__':
     app.run(debug=True)
 
